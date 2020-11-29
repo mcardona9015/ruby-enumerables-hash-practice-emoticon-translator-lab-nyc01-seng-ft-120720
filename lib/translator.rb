0a07f1a70ba21d9843c28875fb10273372d42e6a
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 04:45:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,11 +13,10 @@ end
 
 def get_japanese_emoticon(file_path, emote)
   emotes = load_library(file_path)
-  binding.pry
+binding.pry
   # code goes here
 end
 
 def get_english_meaning
   # code goes here
 end
-binding.pry
