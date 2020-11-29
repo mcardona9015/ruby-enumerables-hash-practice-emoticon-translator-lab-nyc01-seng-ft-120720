9c47822bcac82b71bda00ff3b93dbd06917f7b5d
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 05:05:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,16 @@ def load_library(file_path)
 end
 emotes = load_library("lib/emoticons.yml")
 binding.pry
-def get_japanese_emoticon(file_path, emote)
+def get_japanese_emoticon(file_path, search_emote)
   emotes = load_library(file_path)
+  return_emote = ""
+  emotes.each do |name, languages|
+    languages.do |language, emote|
+      if emote == search_emote
+        return_emote = languages[:japanese]
+      end
+    end
+  end
   # code goes here
 end
 
