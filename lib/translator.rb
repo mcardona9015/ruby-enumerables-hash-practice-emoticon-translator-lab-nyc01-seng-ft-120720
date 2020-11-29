b2f3f8b2c7e5917e353c3b86c300cecd4d747f15
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 03:39:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,10 @@ require 'pry'
 
 def load_library(file_path)
   emotes = YAML.load(File.read(file_path))
-binding.pry
   emotes.each_with_object({}) do |key, value|
     value = {:english => value[0], :japanese => value[1]}
     {key => value}
+    binding.pry
   end
   # code goes here
 end
