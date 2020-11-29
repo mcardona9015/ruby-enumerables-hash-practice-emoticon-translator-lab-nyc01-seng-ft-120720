4f61a00fd35d222a791fe36c6a846647a933057f
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 04:03:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,11 @@ require 'pry'
 emotes = YAML.load(File.read('lib/emoticons.yml'))
 def load_library(file_path)
   emotes = YAML.load(File.read(file_path))
+  new_emotes = {}
   emotes.map do |key, value|
     value = {:english => value[0], :japanese => value[1]}
-    {key => value}
-  emotes.flatten
+    new_emotes << {key => value}
+  
 
   end
   # code goes here
