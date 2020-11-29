3aacfddfc535eafe5c44019c274f2d805b876744
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 03:51:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   emotes = YAML.load(File.read(file_path))
   emotes.map do |key, value|
     value = {:english => value[0], :japanese => value[1]}
-    {key => value}.to_h
+    [key => value].to_h
   binding.pry
   end
   # code goes here
