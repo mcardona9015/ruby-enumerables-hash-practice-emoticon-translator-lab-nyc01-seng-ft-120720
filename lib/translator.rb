ed197731369ac72f6f1a82eee1e2a3577751e787
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 05:34:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,7 @@ def get_japanese_emoticon(file_path, search_emote)
     return "Sorry, that emoticon was not found"
   else
     return_emote
+  end
 end
 
 def get_english_meaning(file_path, search_emote)
